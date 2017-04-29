cd3af28b56c9585d6e5fa439c284e27abc29b6cf
jonathanheemstra <jonathan.heemstra@gmail.com>
2017-04-28 17:15:01 -0700

    build out hero header component

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,21 +2,18 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    svg: {
-      paths: [
-        'node_modules/ember-kit/public/icons',
-      ],
-    },
+  const app = new EmberApp(defaults, {
 
     'ember-kit': {
       libraries: [],
 
       adapters: [
+        'app',
+        'audio',
         'image',
+        'list',
         'list-feature',
         'list-row',
-        'list',
         'media-item',
         'media-series',
         'services/kit-accounts-service',
@@ -24,50 +21,73 @@ module.exports = function(defaults) {
         'services/kit-files-service',
         'services/kit-media-service',
         'services/kit-notifications-service',
-        'web-app'
+        'search-hit',
+        'video',
+        'web-app',
+        'web-tab',
       ],
 
       components: [
         'kit-app-lockup',
+        'kit-button',
+        'kit-flat-input',
+        'kit-hero-header-text',
+        'kit-hero-header',
         'kit-icon',
         'kit-image',
-        'kit-list-item',
         'kit-menu-item',
         'kit-nav-item',
         'kit-navbar',
+        'kit-overlay',
+        'kit-panel',
         'kit-search',
         'kit-search-result',
-        'kit-panel',
-        'kit-overlay',
-        'kit-grid-item',
         'yield-for',
       ],
 
-      helpers: [],
+      helpers: [
+        'filter-by',
+        'find-by',
+        'image-url'
+      ],
 
       mixins: [
         'kit-body-click-mixin',
       ],
 
       models: [
+        'app',
+        'audio',
+        'branding',
+        'branding-silhouette-logo',
         'image',
+        'list',
         'list-feature',
         'list-row',
-        'list',
         'media-item',
         'media-series',
-        'web-app'
+        'search-hit',
+        'video',
+        'web-app',
+        'web-tab',
       ],
 
       serializers: [
         'kit-hal',
+        'search-hit',
       ],
 
       services: [
         'kit-event-bus',
+        'tokens',
       ],
-    }
+    },
 
+    svg: {
+      paths: [
+        'node_modules/ember-kit/public/icons',
+      ],
+    },
   });
 
   return app.toTree();
